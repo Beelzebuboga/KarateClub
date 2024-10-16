e1e60350dbab264f6bbc6d6602970793bcc545f3
Beelzebuboga <abdulkhaliqsolaiman@gmail.com>
2024-10-16 11:28:45 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function formSubmit(){
+    var x = 'Thank you for messaging...';
+    document.getElementById('form').innerHTML = x;
+    form.style.color = "white";
+    form.style.fontFamily = "fantasy";
+    form.style.fontSize = "40px";
+    form.style.marginTop = "40px";
+}
